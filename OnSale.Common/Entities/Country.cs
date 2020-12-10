8d8ab1c77f289ed21c087062f99bfb87546f30d8
DIEGO FERNANDO ALVAREZ VARELA <diegoalvarez286818@correo.itm.edu.co>
2020-12-10 14:52:04 -0500

    Improve  countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [MaxLength (50, ErrorMessage = "The filed {0} must contair less than {1} characteres.")]
+        [Required]
+        public String Name { get; set; }
+    }
+}
