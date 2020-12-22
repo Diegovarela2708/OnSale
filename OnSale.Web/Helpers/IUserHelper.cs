844872f7b7e063a3a90965ff999d98fca1376fc3
DIEGO FERNANDO ALVAREZ VARELA <diegoalvarez286818@correo.itm.edu.co>
2020-12-22 15:09:08 -0500

    Recover Password

--- OnSale.Web/Helpers/IUserHelper.cs
+++ OnSale.Web/Helpers/IUserHelper.cs
@@ -28,6 +28,21 @@ namespace OnSale.Web.Helpers
 
         Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, UserType userType);
 
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<User> GetUserAsync(Guid userId);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+
 
     }
 }
